9c8300d059c9e6b1f6519752839179102d4aaa44
Ove <ove.kristian.jorgensen@gmail.com>
2022-04-12 14:53:31 +0200

    Able to post multiple designs with names

--- ar_for_education_frontend/src/axios/service.ts
+++ ar_for_education_frontend/src/axios/service.ts
@@ -1,8 +1,13 @@
 import axios from 'axios';
-import { UserDesignApiData } from '../types';
+import { UserDesign, UserDesignApiData } from '../types';
 import { API_URL } from '../../constants';
 
-export const postUserDesign = async (userDesign: UserDesignApiData) => {
-    const response = await axios.post(`${API_URL}/api/user-designs`, userDesign);
-    return response.data;
+export const postUserDesign = async (userDesign: UserDesign) => {
+    try {
+        const response = await axios.post<UserDesignApiData>(`${API_URL}/api/user-designs`, userDesign);
+        return response.data || [];
+    }
+    catch (error) {
+        throw new Error(`Error while posting user design: ${error}`);
+    }
 };
