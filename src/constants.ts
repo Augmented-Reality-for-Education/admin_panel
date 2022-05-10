b891301f535a15d9ca34647539bfdd72de232951
Marius Sørensen <jobb@s0rensen.no>
2022-05-10 17:40:45 +0200

    Use correct constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
-export const API_URL: string = "https://ar-for-education-api.azurewebsites.net/Image"; 
+export const API_URL: string = "https://ar-for-education-api.azurewebsites.net";
+// export const API_URL = "https://localhost:44372";
